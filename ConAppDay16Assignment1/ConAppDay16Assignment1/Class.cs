1218e01b1f91b2c16b0ce0657b57f8cdde677eb5
ashwithans <ashwithans@gmail.com>
2023-11-27 15:51:12 +0530

    Assignmnet_day16_part1&part2

---
+++ ConAppDay16Assignment1/ConAppDay16Assignment1/Class.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppDay16Assignment1
+{
+    public class Class
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtarct(int a, int b)
+        {
+            return a - b;
+        }
+        public int Multi(int a, int b)
+        {
+            return a * b;
+        }
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
